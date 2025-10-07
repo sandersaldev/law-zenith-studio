12dfd050b9a270908e6602699464006e384b9204
sandersaldev <developer.sandersal@gmail.com>
2025-10-07 17:10:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NOME_DO_REPOSITORIO/',  // 👈 coloque o nome EXATO do repo aqui!
+  base: '/advocacia1/',  // 👈 coloque o nome EXATO do repo aqui!
 })
 
